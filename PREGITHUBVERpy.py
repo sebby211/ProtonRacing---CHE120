2f244c41d65d7b873253b0903cc96051ad351559
Blaze0607 <jackvincent208@gmail.com>
2024-12-01 21:38:51 -0500

    Update PREGITHUBVERpy.py

--- PREGITHUBVERpy.py
+++ PREGITHUBVERpy.py
@@ -37,37 +37,41 @@ car_x, car_y = 968, 225
 crash_sound = pygame.mixer.Sound('gameoversoundeffect.mp3') 
 start_time = None
 
-class Car:
-    def __init__(self, pygame):
-        self.sizey = 20
-        self.sizex = 50
-        self.position = pygame.Vector2(1075, 350)
-        self.shape = pygame.image.load('carnew.png')
-        self.image = self.shape
-        self.velocity = 0
-        self.acceleration = 0.006
-        self.rotation = 0
-        self.cornering = 0.4
-        self.topspeed = 2
-        
-    def update_position(self, math):
+class Car: #JV: Initializes a car class which can be used to create car objects 
+    def __init__(self, pygame): #JV: Initialization of the parts of the class
+        self.sizey = 20 #JV: A size in the vertical direction
+        self.sizex = 50 #JV: A size in the horizontal direction
+        self.position = pygame.Vector2(1075, 350) #JV: Establish a vector position
+        self.shape = pygame.image.load('carnew.png') #JV: Loads the attached image from the zip file
+        self.image = self.shape #JV: Creates a image that can be transformed without changing the original image
+        self.velocity = 0 #JV: Velocity of car at different times, starts at 0 because car doesn't move
+        self.acceleration = 0.006 #JV: The rate that the velocity goes up per tick of the code, many ticks per second so value is very small
+        self.rotation = 0 #JV: Current angle relative to horizontal of the car
+        self.cornering = 0.4 #JV: Amount the cars rotation can change per tick, in degrees.
+        self.topspeed = 2 #JV: Maximum speed the car can hit
+
+    #JV: Note: Code did not behave as expected initially because when I wrote it in my environment, point (0,0) was bottom left
+    #JV: Cont: For the code written by other members it was the top left
+    #JV: Cont: This resulted in weird results where in order to increase velocity we need to actually decrease it etc.
+    #JV: Cont: Basically, I just inverted all of the statements until it worked
+    def update_position(self, math): #JV: Function for updating position of the car based on the velocity and rotation. Uses trig. to determine cars position
         self.position = pygame.Vector2(self.position.x + (math.sin(math.radians(self.rotation)))*self.velocity, self.position.y + (math.cos(math.radians(self.rotation))*self.velocity))
-    def update_rotation_left(self):
+    def update_rotation_left(self): #JV: Player choose to go left, so angle responds accordingly
         self.rotation += self.cornering
-    def update_rotation_right(self):
+    def update_rotation_right(self): #JV: Player choose to go right, so angle responds accordingly
         self.rotation -= self.cornering
-    def goforwardvelocity(self):
+    def goforwardvelocity(self): #JV: Player goes forward which changes the velocity as long as its less than the top speed
         if self.velocity >= self.topspeed*(-1):
             self.velocity -= self.acceleration
-    def gobackwardvelocity(self):
+    def gobackwardvelocity(self): #JV: Player goes backward, velocity is changed as long as not going backward faster than top speed
         if self.velocity <= self.topspeed:
             self.velocity += self.acceleration
-    def update_image(self, math, pygame):
+    def update_image(self, math, pygame): #JV: Blits to the screen a copy of the the current positon (image) of the car.
         self.update_position(math)
         image = pygame.transform.rotate(self.image, self.rotation)
         screen.blit(image, self.position)
                     
-n = Car(pygame)
+n = Car(pygame) #JV: Initializes a car object for the player
 
 def start_count_down():
     count_down_messages = ["3", "2", "1", "Go!"]
@@ -78,36 +82,36 @@ def start_count_down():
         pygame.display.update()  
         time.sleep(1)  
 global times_list
-times_list = [0]   
+times_list = [0]  #JV: Initially there are no times, but needs some text to print or there will be an error, hence: 0
 
-def display_timer(carobject, lap, moved, start_time):
-    if start_time is not None:
-        elapsed_time = time.time() - start_time
-        minutes = int(elapsed_time) // 60
+def display_timer(carobject, lap, moved, start_time): #JV: Defines a function with required parameters
+    if start_time is not None: #JV: Insuring the game has started
+        elapsed_time = time.time() - start_time #JV: Time of start minus the current time represents the elapsed time
+        minutes = int(elapsed_time) // 60 #JV: If time is > 60, will need to be split into minutes and seconds
         seconds = int(elapsed_time) % 60
-        timer_text = f"{minutes:02}:{seconds:02}"
-        text = small_font.render(timer_text, True, WHITE)
-        screen.blit(text, (20, 20))
-        x,y = carobject.position
-        if int(x) > 1000 and int(x) < 1150 and int(y) == 350 and moved:
-            if times_list[0] == 0:
-                times_list[0] = elapsed_time
-            times_list.append(elapsed_time)
-            start_time = time.time()
-            lap += 1
-            moved = False
-        lap_text = small_font.render('LAP: ' + str(lap), True, WHITE)
-        screen.blit(lap_text, (1050, 10))
-        w = min(times_list)
-        w = ("%.2f" % w)
-        w = str(w)
-        w = w.split('.')
-        if w[0] == 0:
+        timer_text = f"{minutes:02}:{seconds:02}" 
+        text = small_font.render(timer_text, True, WHITE) #JV: Renders the text with font on the screen for the timer
+        screen.blit(text, (20, 20)) #JV: Blits it to the screen
+        x,y = carobject.position #JV: Takes the x,y position of the car currently 
+        if int(x) > 1000 and int(x) < 1150 and int(y) == 350 and moved: #JV: If car is currently at the start line and has moved since the last it was there
+            if times_list[0] == 0: #JV: Times list was initialized with a 0, if it still contains it, remove it and put the new time at its index
+                times_list[0] = elapsed_time #JV: If 0 stays, it will always be the quickest lap, must be removed
+            times_list.append(elapsed_time) #JV: if not 0, just append it to the list
+            start_time = time.time() #JV: Redefine start time as now for future laps
+            lap += 1 #JV: Increment laps
+            moved = False #JV: The car has not moved since the last time it was at the start, so False. When it moves again it will be set to true
+        lap_text = small_font.render('LAP: ' + str(lap), True, WHITE) #JV: Display the text of the current lap
+        screen.blit(lap_text, (1050, 10)) #JV: Blits it to the screen
+        w = min(times_list) #JV: Finds the minimum/best lap time
+        w = ("%.2f" % w) #JV: Formats it to 2 decimal places
+        w = str(w) #JV: Turns into a str so it can be printed
+        w = w.split('.') #JV: Remove the period and split it into a list
+        if w[0] == 0: #JV: Only pertinent for 1st lap, just reformates the 0 for the first lap
             w = ['0','00']
-        output = 'Best LAP: ' + str(w[0])+':'+ str(w[1])
-        best_text = small_font.render(output, True, WHITE)
-        screen.blit(best_text, (790, 50))
-        return lap, moved, start_time
+        output = 'Best LAP: ' + str(w[0])+':'+ str(w[1]) #JV: Creates a str for output, format is "seconds" + ":" + "milliseconds".
+        best_text = small_font.render(output, True, WHITE) #JV: Renders the text 
+        screen.blit(best_text, (790, 50)) #JV: Blits it to the screen
+        return lap, moved, start_time #JV: Returns information needed for future loops of this function, will be repassed through every time. 
         
 pause_menu_option_y_positions = [360, 420]
 
@@ -258,10 +262,10 @@ def main():
                             screen.blit(track_image, (0, 0)) 
                            
                             Car.update_image(n, math, pygame)
-                            a = display_timer(n, lap, moved, start_time)
-                            lap = a
-                            pygame.display.update() 
-                            return lap
+                            a = display_timer(n, lap, moved, start_time) #JV: n is the car object, lap is the current lap, moved is whether the car has moved
+                            lap = a #JV: Sets lap to return of display_timer to see if lap has changed
+                            pygame.display.update() #JV: Prints to screen all rendered/blitted stuff throughout the code
+                            return lap #Returns lap for running throughout the loop
                             
 
                         def run_game(lap, carobject): 
@@ -292,9 +296,9 @@ def main():
                                 if keys[pygame.K_d]:
                                     Car.update_rotation_right(n)
                                  
-                                x, y = carobject.position
-                                if int(x) < 1000 or int(x) > 1150 and int(y) != 350:
-                                    moved = True
+                                x, y = carobject.position #JV: Takes the cars position
+                                if int(x) < 1000 or int(x) > 1150 and int(y) != 350: #JV: Sees if the car has moved
+                                    moved = True #JV: Car has moved, so set moved = True
                                 
                                 if not is_on_track(n):
                                     crash_sound.play()
@@ -302,7 +306,7 @@ def main():
                                     game_running = False 
                                     gameover_menu()
                                 
-                                lap, moved, start_time = render_objects(lap, moved, start_time) 
+                                lap, moved, start_time = render_objects(lap, moved, start_time) #JV: Passes in lap, moved, and start_time, and will return them if they have changed
                                 
                                 pygame.display.flip() 
                                 
